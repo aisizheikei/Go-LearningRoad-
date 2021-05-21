c03d5711bca7f21b60c0753c2f0401a45e1aa573
sjk <1746870208@qq.com>
2021-05-21 08:18:43 +0800

    time相关学习

---
+++ Go/src/github.com/defiabell/Golang/10Time/10Time.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println("时间：", now)
+
+	//时间戳
+	fmt.Println("毫秒时间戳：", now.Unix())
+	fmt.Println("纳秒时间戳：", now.UnixNano())
+
+	// //定时器
+	// timer := time.Tick(time.Second)
+	// //每秒执行一次
+	// for t := range timer {
+	// 	fmt.Println(t)
+	// }
+
+	//时间格式化 Go语言不同于其他语言，格式化不是通过'YY-mm-dd'。而是用'2006-01-02 03:04:05'，也就是Go语言的诞生时间
+	fmt.Println(now.Format("2006-01-02 03:04:05"))
+	fmt.Println(now.Format("2006-01-02 15:04:05"))
+	fmt.Println(now.Format("2006-01-02 15:04:05.000"))
+
+	//时间计算
+	fmt.Println(now.Add(1 * time.Hour))
+	timeTemp, err := time.Parse("2006/01/02", "2019/02/20")
+	if err != nil {
+		fmt.Println("时间转换失败")
+	}
+	fmt.Println(now.Sub(timeTemp))
+	fmt.Println("----------------------------------------------")
+
+}
